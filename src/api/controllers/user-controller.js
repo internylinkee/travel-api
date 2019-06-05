ad90e6d3897332a29b7edd5d861332b3914c7649
Minh Ngo <nvminh023@gmail.com>
2019-06-05 11:24:06 +0700

    create register user api

--- src/api/controllers/user-controller.js
+++ src/api/controllers/user-controller.js
@@ -1,16 +1,25 @@
+const httpStatus = require('http-status');
+const bcrypt = require('bcrypt');
 const User = require('../models/user-model');
 
-module.exports.create = async (req, res, next) => {
-  const user = await User.findById('5cf624afdad76d11bc4242dd').populate({
-    path: 'followers',
-  });
-  // const newUser = new User({
-  //   fullName: {
-  //     firstName: 'aaaaa',
-  //   },
-  // });
-  // await newUser.save();
-  console.log(req.body);
+module.exports.register = async (req, res, next) => {
+  const { email, password, firstName, lastName } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(
+      password,
+      parseInt(process.env.SALT_ROUND),
+    );
 
-  res.json(user);
+    const newUser = await User.create({
+      email,
+      password: hashedPassword,
+      fullName: {
+        firstName,
+        lastName,
+      },
+    });
+    return res.status(httpStatus.CREATED).json(newUser);
+  } catch (err) {
+    next(err);
+  }
 };
--- src/api/routes/user-route.js
+++ src/api/routes/user-route.js
@@ -4,6 +4,6 @@ const userController = require('../controllers/user-controller');
 const { register } = require('../validations/user.validate');
 const route = express.Router();
 
-route.post('/register', validate(register), userController.create);
+route.post('/register', validate(register), userController.register);
 
 module.exports = route;
