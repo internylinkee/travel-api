13e8ed15a0c32b8a788f5b0f335cb00f6021defd
Minh Ngo <nvminh023@gmail.com>
2019-06-14 09:20:32 +0700

    create create review api

--- src/api/controllers/user-controller.js
+++ src/api/controllers/user-controller.js
@@ -1,6 +1,7 @@
 const httpStatus = require('http-status');
 const User = require('../models/user-model');
 const Notification = require('../models/notification-model');
+const Review = require('../models/review-model');
 
 module.exports.get = async (req, res, next) => {
   try {
@@ -125,6 +126,55 @@ module.exports.follow = async (req, res, next) => {
     return res.status(httpStatus.OK).json({
       likeCount: updatedUser.followers.length,
     });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.review = async (req, res, next) => {
+  const {
+    user,
+    params: { id },
+    body: { text, rating },
+  } = req;
+  try {
+    const tourGuide = await User.findById(id);
+    if (!tourGuide.isTourGuide) {
+      throw new Error(
+        'Bạn không thể đánh giá user mà không phải là hướng dẫn viên.',
+      );
+    }
+
+    const isExistreview = await Review.findOne({ user, tourGuide })
+      .select('_id')
+      .lean();
+    if (isExistreview) {
+      throw new Error('Bạn đã đánh giá về hướng dẫn viên này.');
+    }
+
+    const review = await Review.create({
+      user,
+      text,
+      rating,
+      tourGuide,
+    });
+
+    const [{ reviewCount, avgRating }] = await Review.aggregate([
+      { $match: { tourGuide: tourGuide._id } },
+      {
+        $group: {
+          _id: null,
+          reviewCount: { $sum: 1 },
+          avgRating: { $avg: '$rating' },
+        },
+      },
+    ]);
+
+    tourGuide.tourGuideProfile.rating = avgRating;
+    tourGuide.tourGuideProfile.reviewCount = reviewCount;
+    await tourGuide.save();
+
+    return res.status(httpStatus.CREATED).json(review);
   } catch (err) {
     next(err);
   }
