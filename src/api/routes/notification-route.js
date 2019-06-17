086de7490eb5fd6953df50a3632e3e5cc4c0077d
Minh Ngo <nvminh023@gmail.com>
2019-06-17 09:29:27 +0700

    mark as read notification api

--- src/api/routes/notification-route.js
+++ src/api/routes/notification-route.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const validate = require('express-validation');
 const notifyControllers = require('../controllers/notification-controller');
+const notifyValidations = require('../validations/notification-validate');
 const { authorize } = require('../middlewares/auth');
 const { paginate } = require('../middlewares/paginate');
 
 const router = express.Router();
 
 router.route('/').get(authorize(), paginate(), notifyControllers.getAll);
+
 router.route('/count-unread').get(authorize(), notifyControllers.countUnread);
+
+router
+  .route('/:id/read')
+  .put(
+    validate(notifyValidations.markAsRead),
+    authorize(),
+    notifyControllers.markAsRead,
+  );
 
 module.exports = router;
