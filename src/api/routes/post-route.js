feaeeb24b08cb9612208dd8b97dc1fb646cbdfe2
Minh Ngo <nvminh023@gmail.com>
2019-07-03 09:31:06 +0700

    create docs for post apis

--- src/api/routes/post-route.js
+++ src/api/routes/post-route.js
@@ -17,8 +17,6 @@ router
    *  @swagger
    *  /posts:
    *    get:
-   *      security:
-   *      - bearerAuth: []
    *      tags:
    *      - Post
    *      summary: Get list of post
@@ -134,7 +132,26 @@ router
     postControllers.create,
   );
 
-router.route('/hot').get(authorize(), postControllers.getHotPost);
+/**
+ *  @swagger
+ *  /posts/hot:
+ *    get:
+ *      tags:
+ *      - Post
+ *      summary: Get a list of hit posts
+ *      description: Get top 10 hit posts
+ *      responses:
+ *        200:
+ *          description: OK
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/commponents/schemas/Post'
+ *
+ */
+router.route('/hot').get(postControllers.getHotPost);
 
 router
   .route('/:id')
@@ -142,8 +159,6 @@ router
    * @swagger
    * /posts/{id}:
    *    get:
-   *      security:
-   *      - bearerAuth: []
    *      tags:
    *      - Post
    *      summary: Get detail a post
@@ -224,6 +239,44 @@ router
    *                  $ref: '#/components/noAuthToken'
    */
   .put(validate(postValidations.update), authorize(), postControllers.update)
+  /**
+   *  @swagger
+   *  /posts/{id}:
+   *    delete:
+   *      security:
+   *      - bearerAuth: []
+   *      tags:
+   *      - Post
+   *      summary: Delete a post
+   *      description: Delete a post by post's id
+   *      parameters:
+   *      - in: path
+   *        name: id
+   *        required: true
+   *        description: The post id
+   *        schema:
+   *          type: string
+   *          example: 5d11989c40706890cade2228
+   *      responses:
+   *        200:
+   *          description: OK
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message:
+   *                    type: string
+   *                    example: Xoá bài viết thành công.
+   *        400:
+   *          description: Bad request
+   *        401:
+   *          description: No auth token
+   *          content:
+   *            application/json:
+   *              schema:
+   *                $ref: '#/components/noAuthToken'
+   */
   .delete(
     validate(postValidations.delete),
     authorize(),
@@ -232,16 +285,137 @@ router
 
 router
   .route('/:id/like')
+  /**
+   *  @swagger
+   *  /posts/{id}/like:
+   *    put:
+   *      security:
+   *      - bearerAuth: []
+   *      tags:
+   *      - Post
+   *      summary: Like or unlike a post
+   *      description: Like or unlike a post by post id
+   *      parameters:
+   *      - in: path
+   *        name: id
+   *        required: true
+   *        description: The post id
+   *        schema:
+   *          type: string
+   *          example: 5d11989c40706890cade2228
+   *      responses:
+   *        200:
+   *          description: OK
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  likeCount:
+   *                    type: number
+   *        400:
+   *          description: Bad request
+   *        401:
+   *          description: No auth token
+   *          content:
+   *            application/json:
+   *              schema:
+   *                $ref: '#/components/noAuthToken'
+   */
   .put(validate(postValidations.like), authorize(), postControllers.like);
 
 router
   .route('/:id/comments')
+  /**
+   *  @swagger
+   *  /posts/{id}/comments:
+   *    get:
+   *      security:
+   *      - bearerAuth: []
+   *      tags:
+   *      - Post
+   *      summary: Get list of comments of the post
+   *      description: Get list of comments by id and implement pagination
+   *      parameters:
+   *      - in: path
+   *        name: id
+   *        description: The post id
+   *        required: true
+   *        schema:
+   *          type: string
+   *          example: 5d11989c40706890cade2228
+   *      - in: query
+   *        name: page
+   *        description: The page number
+   *        schema:
+   *          type: int
+   *          example: 1
+   *      responses:
+   *        200:
+   *          description: OK
+   *          content:
+   *            application/json:
+   *              schema:
+   *                $ref: '#/components/schemas/Comment'
+   *        400:
+   *          description: Bad request
+   *        401:
+   *          description: No auth token
+   *          content:
+   *            application/json:
+   *              schema:
+   *                $ref: '#/components/noAuthToken'
+   */
   .get(
     validate(postValidations.getListComments),
     authorize(),
     paginate(),
     postControllers.getListComments,
   )
+  /**
+   *  @swagger
+   *    /posts/{id}/comments:
+   *      post:
+   *        security:
+   *        - bearerAuth: []
+   *        tags:
+   *        - Post
+   *        summary: Create a comment belong in a specify post
+   *        description: Create a comment belong in a specify post
+   *        parameters:
+   *        - in: path
+   *          name: id
+   *          description: The post id
+   *          required: true
+   *          schema:
+   *            type: string
+   *            example: 5d11989c40706890cade2228
+   *        requestBody:
+   *          required: true
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  text:
+   *                    type: string
+   *                    example: Hay qua
+   *        responses:
+   *          200:
+   *            description: OK
+   *            content:
+   *              application/json:
+   *                schema:
+   *                  $ref: '#/components/schemas/Comment'
+   *          400:
+   *            description: Bad request
+   *          401:
+   *            description: No auth token
+   *            content:
+   *              application/json:
+   *                schema:
+   *                  $ref: '#/components/noAuthToken'
+   */
   .post(
     validate(postValidations.createComment),
     authorize(),
