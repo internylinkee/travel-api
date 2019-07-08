ac9a7832fe629a7a5660d26911f6ba93d4700f1c
Minh Ngo <nvminh023@gmail.com>
2019-07-08 13:36:37 +0700

    create script to import locations data

---
+++ src/tools/import-categories.js
@@ -0,0 +1,31 @@
+const mongoose = require('../config/mongoose');
+const Category = require('../api/models/category-model');
+
+mongoose.connect();
+
+const importCategories = async () => {
+  try {
+    const categoies = [
+      { name: 'Ăn uống' },
+      { name: 'Du lịch' },
+      { name: 'Giải trí' },
+      { name: 'Mua sắm' },
+      { name: 'Dịch vụ' },
+      { name: 'Món ngon mới lạ' },
+    ];
+
+    await Category.insertMany(categoies);
+  } catch (err) {
+    throw err;
+  }
+};
+
+importCategories()
+  .then(() => {
+    console.log('Done');
+    process.exit(-1);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(-1);
+  });
---
+++ src/tools/import-locations.js
@@ -0,0 +1,28 @@
+const mongoose = require('../config/mongoose');
+const axios = require('axios');
+const Location = require('../api/models/location-model');
+
+mongoose.connect();
+
+const getLocation = async () => {
+  try {
+    let cityList = await axios.get('https://thongtindoanhnghiep.co/api/city');
+    cityList = cityList.data.LtsItem;
+    cityList.pop();
+
+    const cityName = cityList.map(city => ({ name: city.Title }));
+    await Location.insertMany(cityName);
+  } catch (err) {
+    throw err;
+  }
+};
+
+getLocation()
+  .then(() => {
+    console.log('Done');
+    process.exit(-1);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(-1);
+  });
